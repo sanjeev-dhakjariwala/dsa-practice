aa5dd39f08dfa5d3c1e1bf77e23a6c1b6af6e743
Sanjeev Jha <sanjeevjha882@gmail.com>
2023-12-13 05:56:54 +0530

    Recursion tracing code added for CoinChage

--- dp/322CoinChange.py
+++ dp/322CoinChange.py
@@ -11,24 +11,30 @@ class Solution:
     #             if a - c >= 0:
     #                 dp[a] = min(dp[a], 1 + dp[a - c])
     #     return dp[amount] if dp[amount] != amount + 1 else -1
-    def coinChange(self, coins, amount):
-        def recursive_helper(amount):
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        def helper(amount, depth):
+            print("  " * depth, f"Calling helper with amount={amount}")
             if amount == 0:
+                print("  " * depth, "Amount is 0, returning 0")
                 return 0
             if amount < 0:
+                print("  " * depth, "Amount is negative, returning inf")
                 return float('inf')
 
-            min_coins = float('inf')
+            min_coin = float('inf')
             for coin in coins:
-                subproblem = recursive_helper(amount - coin)
-                if subproblem != float('inf'):
-                    min_coins = min(min_coins, subproblem + 1)
+                print("  " * depth, f"Exploring coin={coin}")
+                ans = helper(amount - coin, depth + 1)
+                if ans != float('inf'):
+                    min_coin = min(min_coin, 1 + ans)
+                print("  " * depth, f"Min coin after coin={coin}: {min_coin}")
 
-            return min_coins
+            print("  " * depth, f"Returning min_coin: {min_coin}")
+            return min_coin
 
-        result = recursive_helper(amount)
+        print("Recursion calls:")
+        result = helper(amount, 0)
         return result if result != float('inf') else -1
-
 
 sol = Solution()
 print(sol.coinChange([1, 2, 5], 11))
