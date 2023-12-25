679e05ed20d74448b4bebcaa4b7d1d1608922b37
Sanjeev Jha <sanjeevjha882@gmail.com>
2023-12-26 01:45:12 +0530

    26-DEC-23

--- binary_search/33SearchInRotatedSortedArray.py
+++ binary_search/33SearchInRotatedSortedArray.py
@@ -3,22 +3,25 @@ from typing import *
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        low, high = 0, len(nums) - 1
+        l, r = 0, len(nums) - 1
 
-        while low <= high:
-            mid = low + (high - low) // 2
-            if nums[mid] == target:
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
                 return mid
-            elif nums[mid] >= nums[low]:
-                if nums[mid] < target or nums[low] > target:
-                    low = mid + 1
+
+            # left sorted portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
                 else:
-                    high = mid - 1
+                    r = mid - 1
+            # right sorted portion
             else:
-                if nums[mid] < target or nums[low] > target:
-                    low = mid + 1
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
                 else:
-                    high = mid - 1
+                    l = mid + 1
         return -1
 
 
